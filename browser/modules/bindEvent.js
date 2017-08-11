c71a9e6250ea47bdb64bb88284e617a0f9051691
Martin Høgh <mh@mapcentia.com>
2017-08-11 14:05:49 +0200

    stuff

--- browser/modules/bindEvent.js
+++ browser/modules/bindEvent.js
@@ -228,11 +228,6 @@ module.exports = module.exports = {
         });
 
         backboneEvents.get().on("doneLoading:layers", function (e) {
-            console.log("doneLoading:layers triggered");
-
-            console.log(layers.getCountLoading());
-            console.log(loadingB);
-
             if (layers.getCountLoading() === 0) {
                 layers.resetCount();
                 doneL = true;
