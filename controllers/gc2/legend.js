374e6eff8a26a7a8a7a76f20a296dd39c60b7a59
Martin Høgh <mh@mapcentia.com>
2017-08-31 13:19:02 +0200

    Handle of errors

--- controllers/gc2/legend.js
+++ controllers/gc2/legend.js
@@ -8,19 +8,17 @@ router.get('/api/legend/:db', function (req, response) {
     var l = req.query.l, db = req.params.db, url, jsfile;
     url = config.host + "/api/v1/legend/json/" + db + "?l=" + l;
 
-    request.get(url)
-
-        .on('response', function (res) {
-            if (res.statusCode !== 200) {
-                response.header('content-type', 'application/json');
-                response.status(400).send({
-                    success: false,
-                    message: "Could not get the legend data."
-                });
-            } else {
-                response.send((res));
-            }
-        })
+    request.get(url, function (err, res, body) {
+        if (res.statusCode !== 200) {
+            response.header('content-type', 'application/json');
+            response.status(400).send({
+                success: false,
+                message: "Could not get the legend data."
+            });
+            return;
+        }
+        response.send(JSON.parse(body));
+    })
 
 
 });
