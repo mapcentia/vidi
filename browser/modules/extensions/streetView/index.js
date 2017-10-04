32e8ab841eb62af5ce35d2b1cd970eb65d07d981
Martin Høgh <mh@mapcentia.com>
2017-10-04 14:09:23 +0200

    Pure reactjs

--- browser/modules/extensions/streetView/index.js
+++ browser/modules/extensions/streetView/index.js
@@ -35,7 +35,15 @@ var transformPoint;
  */
 var exId = "streetview";
 
+/**
+ *
+ */
+var clicktimer;
 
+/**
+ *
+ */
+var mapObj;
 
 
 /**
@@ -63,6 +71,8 @@ module.exports = module.exports = {
      */
     init: function () {
 
+        mapObj = cloud.get().map;
+
         /**
          *
          */
@@ -73,32 +83,40 @@ module.exports = module.exports = {
          */
         var ReactDOM = require('react-dom');
 
+        /**
+         *
+         * @type {{Info: {da_DK: string, en_US: string}, Street View: {da_DK: string, en_US: string}, Choose service: {da_DK: string, en_US: string}, Activate: {da_DK: string, en_US: string}}}
+         */
         var dict = {
 
-            "Info" : {
+            "Info": {
                 "da_DK": "Start Google Street View eller Mapillary op fra hvor du klikker i kortet. Servicen starter i et nyt browser vindue.",
                 "en_US": "Start Google Street View or Mapillary from where you click on the map. The service starts in a new browser window."
             },
 
-            "Street View" : {
+            "Street View": {
                 "da_DK": "Gadefoto",
                 "en_US": "Street View"
             },
 
-            "Choose service" : {
+            "Choose service": {
                 "da_DK": "Vælg service",
                 "en_US": "Choose service"
             },
 
-            "Activate" : {
+            "Activate": {
                 "da_DK": "Aktiver",
                 "en_US": "Activate"
             }
         };
 
-        var __ = function(txt) {
-
-
+        /**
+         *
+         * @param txt
+         * @returns {*}
+         * @private
+         */
+        var __ = function (txt) {
             if (dict[txt][window._vidiLocale]) {
                 return dict[txt][window._vidiLocale];
             } else {
@@ -112,34 +130,144 @@ module.exports = module.exports = {
         class Streetview extends React.Component {
             constructor(props) {
                 super(props);
+
+                this.state = {
+                    active: false,
+                    selectedOption: "google"
+                };
+
+                this.onActive = this.onActive.bind(this);
+                this.onChange = this.onChange.bind(this);
             }
 
+            /**
+             *
+             * @param e
+             */
+            onActive(e) {
+                this.setState({
+                    active: e.target.checked
+                });
+
+                if (e.target.checked) {
+
+                    // Turn info click off
+                    //====================
+                    backboneEvents.get().trigger("off:infoClick");
+
+                    // Emit "on" event
+                    //================
+                    backboneEvents.get().trigger("on:" + exId);
+
+                    utils.cursorStyle().crosshair();
+
+                } else {
+
+                    // Turn info click on again
+                    //=========================
+                    backboneEvents.get().trigger("on:infoClick");
+
+                    // Emit "off" event
+                    //=================
+                    backboneEvents.get().trigger("off:" + exId);
+
+                    utils.cursorStyle().reset();
+
+                }
+
+            }
+
+            onChange(changeEvent) {
+                this.setState({
+                    selectedOption: changeEvent.target.value
+                });
+            }
+
+            /**
+             *
+             */
+            componentDidMount() {
+                var me = this;
+
+                // Handle click events on map
+                // ==========================
+
+                mapObj.on("dblclick", function () {
+                    clicktimer = undefined;
+                });
+                mapObj.on("click", function (e) {
+                    var event = new geocloud.clickEvent(e, cloud);
+                    if (clicktimer) {
+                        clearTimeout(clicktimer);
+                    }
+                    else {
+                        if (me.state.active === false) {
+                            return;
+                        }
+
+                        clicktimer = setTimeout(function (e) {
+
+                            var coords = event.getCoordinate(), p, url;
+                            p = utils.transform("EPSG:3857", "EPSG:4326", coords);
+                            clicktimer = undefined;
+
+                            switch (me.state.selectedOption) {
+                                case "google":
+                                    url = "http://maps.google.com/maps?q=&layer=c&cbll=" + p.y + "," + p.x + "&cbp=11,0,0,0,0";
+                                    break;
+
+                                case "mapillary":
+                                    url = "https://www.mapillary.com/app/?lat=" + p.y + "&lng=" + p.x + "&z=17";
+                                    break;
+                            }
+
+                            utils.popupCenter(url, (utils.screen().width - 100), (utils.screen().height - 100), exId);
+
+
+                        }, 250);
+                    }
+                });
+            }
+
+            /**
+             *
+             * @returns {XML}
+             */
             render() {
                 return (
 
                     <div role="tabpanel">
                         <div className="panel panel-default">
                             <div className="panel-body">
-                                    <div className="form-group">
-                                        <div className="togglebutton">
-                                            <label><input id="streetview-btn" type="checkbox"/>{__("Activate")}</label>
-                                        </div>
-                                        <h3>{__("Choose service")}</h3>
-                                        <div className="radio">
-                                            <label>
-                                                <input type="radio" id="streetview-service-google" name="streetview-service" value="google" defaultChecked="1"/>
-                                                Google Street View
-                                            </label>
-                                        </div>
-
-                                        <div className="radio">
-                                            <label>
-                                                <input type="radio" id="streetview-service-mapillary" name="streetview-service" value="mapillary"/>
-                                                Mapillary
-                                            </label>
-                                        </div>
+                                <div className="form-group">
+                                    <div className="togglebutton">
+                                        <label><input id="streetview-btn" type="checkbox"
+                                                      defaultChecked={ this.state.active }
+                                                      onChange={this.onActive}/>{__("Activate")}
+                                        </label>
 
                                     </div>
+                                    <h3>{__("Choose service")}</h3>
+                                    <div className="radio">
+                                        <label>
+                                            <input type="radio" id="streetview-service-google" name="streetview-service"
+                                                   value="google" checked={this.state.selectedOption === 'google'}
+                                                   onChange={this.onChange}/>
+                                            Google Street View
+                                        </label>
+                                    </div>
+
+                                    <div className="radio">
+                                        <label>
+                                            <input type="radio" id="streetview-service-mapillary"
+                                                   name="streetview-service" value="mapillary"
+                                                   checked={this.state.selectedOption === 'mapillary'}
+                                                   onChange={this.onChange}/>
+                                            Mapillary
+                                        </label>
+                                    </div>
+
+                                </div>
 
                             </div>
                         </div>
@@ -162,55 +290,6 @@ module.exports = module.exports = {
 
         }
 
-    },
-
-    /**
-     *
-     */
-    control: function () {
-        if ($("#" + exId + "-btn").is(':checked')) {
-
-            // Emit "on" event
-            //================
-
-            backboneEvents.get().trigger("on:" + exId);
-
-            utils.cursorStyle().crosshair();
-
-        } else {
-
-            // Emit "off" event
-            //=================
-
-            backboneEvents.get().trigger("off:" + exId);
-
-            utils.cursorStyle().reset();
-
-        }
-    },
-
-    click: function (event) {
-        var coords = event.getCoordinate(), p, url;
-        p = utils.transform("EPSG:3857", "EPSG:4326", coords);
-
-        switch ($("input:radio[name='streetview-service']:checked").val()) {
-            case "google":
-                url =  "http://maps.google.com/maps?q=&layer=c&cbll=" + p.y + "," + p.x + "&cbp=11,0,0,0,0";
-                break;
-
-            case "mapillary":
-                url = "https://www.mapillary.com/app/?lat=" + p.y + "&lng=" + p.x + "&z=17";
-                break;
-        }
-
-        utils.popupCenter(url, (utils.screen().width - 100), (utils.screen().height - 100), exId);
-    },
-
-    /**
-     * Turns conflict off and resets DOM
-     */
-    off: function () {
-        // Clean up
     }
 
 };
