d51bf9f2a3c82cba3d5edc93ad63268b88c1dc67
Martin Høgh <mh@mapcentia.com>
2018-01-03 13:17:19 +0100

    Handling of unicoded strings

--- controllers/gc2/meta.js
+++ controllers/gc2/meta.js
@@ -3,28 +3,34 @@ var router = express.Router();
 var http = require('http');
 var config = require('../../config/config.js').gc2;
 var request = require('request');
+var utf8 = require('utf8');
 
 router.get('/api/meta/:db/:schema', function (req, response) {
     var db = req.params.db, schema = req.params.schema, url, data = [];
-    url = config.host + "/api/v1/meta/" + db + "/" + schema;
+    url = config.host + "/api/v1/meta/" + db + "/" + utf8.encode(schema);
 
     console.log(url);
 
-    request.get(url, function (err, res, body) {
+    request.get({
+            uri: url,
+            encoding: 'utf8'
+        },
 
-        if (err) {
+        function (err, res, body) {
 
-            response.header('content-type', 'application/json');
-            response.status(400).send({
-                success: false,
-                message: "Could not get the meta data."
-            });
+            if (err) {
 
-            return;
-        }
+                response.header('content-type', 'application/json');
+                response.status(400).send({
+                    success: false,
+                    message: "Could not get the meta data."
+                });
 
-        response.send(JSON.parse(body));
-    })
+                return;
+            }
+
+            response.send(JSON.parse(body));
+        })
 });
 module.exports = router;
 
