0f1e07c4c972fe623f912e2a0e9a4541bcff6a8f
Martin Høgh <mh@mapcentia.com>
2018-02-17 22:09:40 +0100

    extensions

--- extensions/vectorLayers/browser/index.js
+++ extensions/vectorLayers/browser/index.js
@@ -160,7 +160,7 @@ module.exports = {
     },
 
     createLayerTree: function () {
-        var base64name, arr, groups = [], metaData, i, l, displayInfo;
+        var base64name, arr, groups = [], metaData, i, l, displayInfo, subOrderHeader, icon;
 
         metaData = meta.getMetaData();
 
@@ -173,15 +173,26 @@ module.exports = {
         metaData.data.reverse();
 
         for (i = 0; i < arr.length; ++i) {
+
             if (arr[i] && arr[i] !== "<font color='red'>[Ungrouped]</font>") {
                 l = [];
                 base64name = Base64.encode(arr[i]).replace(/=/g, "");
                 $("#vectorlayers").append('<div class="panel panel-default" id="vectorlayer-panel-' + base64name + '"><div class="panel-heading" role="tab"><h4 class="panel-title"><div class="layer-count badge"><span>0</span> / <span></span></div><a style="display: block" class="accordion-toggle" data-toggle="collapse" data-parent="#vectorlayers" href="#vectorcollapse' + base64name + '"> ' + arr[i] + ' </a></h4></div><ul class="list-group" id="vectorgroup-' + base64name + '" role="tabpanel"></ul></div>');
                 $("#vectorgroup-" + base64name).append('<div id="vectorcollapse' + base64name + '" class="accordion-body collapse"></div>');
+
+                console.log(metaData.data);
+
                 for (var u = 0; u < metaData.data.length; ++u) {
                     if (metaData.data[u].layergroup == arr[i]) {
+
                         var text = (metaData.data[u].f_table_title === null || metaData.data[u].f_table_title === "") ? metaData.data[u].f_table_name : metaData.data[u].f_table_title,
                             id = "v:" + metaData.data[u].f_table_schema + "." + metaData.data[u].f_table_name;
+
+                        try {
+                            icon = "<img style='width: 20px;vertical-align: text-bottom;' src='https://webkort.syddjurs.dk/images/custom/map-icons/" + JSON.parse(metaData.data[u].meta).oplev_ikon + "'> ";
+                        } catch (e) {
+                            icon = "";
+                        }
 
                         if (!metaData.data[u].baselayer) {
                             store[id] = new geocloud.sqlStore({
@@ -216,8 +227,12 @@ module.exports = {
 
                                 pointToLayer: pointToLayer[id]
                             });
+
                             displayInfo = (metaData.data[u].meta !== null && $.parseJSON(metaData.data[u].meta) !== null && typeof $.parseJSON(metaData.data[u].meta).meta_desc !== "undefined") ? "inline" : "none";
-                            $("#vectorcollapse" + base64name).append('<li class="layer-item list-group-item"><div class="checkbox"><label class="overlay-label" style="width: calc(100% - 50px);"><input type="checkbox" data-gc2-id-vec="' + id + '">' + text + '</label><span><i class="refresh-vector-layer fa fa-list' +
+
+                            subOrderHeader = (typeof metaData.data[u].extra !== "undefined" && metaData.data[u].extra !== null) ? metaData.data[u].extra : "";
+
+                            $("#vectorcollapse" + base64name).append('<li class="layer-item list-group-item"><div class="layer-sub-order-header">' + subOrderHeader + '</div><div class="checkbox"><label class="overlay-label" style="width: calc(100% - 50px);"><input type="checkbox" data-gc2-id-vec="' + id + '">' + icon + "" + text + '</label><span><i class="refresh-vector-layer fa fa-list' +
                                 '" style="display: inline-block; float: none; cursor: pointer;"></i></span></div></li>');
                             l.push({});
                         }
