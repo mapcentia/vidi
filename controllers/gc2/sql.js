5b2e3268d2014bc2450f5a0016581d4123471720
Martin Høgh <mh@mapcentia.com>
2018-08-20 23:38:28 +0200

    Download of query results

--- controllers/gc2/sql.js
+++ controllers/gc2/sql.js
@@ -12,10 +12,16 @@ router.all('/api/sql/:db', function (req, response) {
         client_encoding = req.body.client_encoding || req.query.client_encoding,
         base64 = req.body.base64 || req.query.base64,
         format = req.body.format || req.query.format,
+        store = req.body.store || req.query.store,
         userName,
-        headers;
-
-    console.log(format)
+        fileName,
+        writeStream,
+        rem,
+        headers,
+        key = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
 
     var postData = "q=" + encodeURIComponent(q) + "&base64=" + (base64 === "true" ? "true" : "false") + "&srs=" + srs + "&lifetime=" + lifetime + "&client_encoding=" + client_encoding + "&format=" + (format ? format : "geojson") + "&key=" + req.session.gc2ApiKey,
         options;
@@ -38,6 +44,7 @@ router.all('/api/sql/:db', function (req, response) {
     };
 
     if (format === "excel") {
+        fileName = key + ".xlsx";
         headers = {
             'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
             'Content-Disposition': 'attachment; filename=data.xlsx',
@@ -46,6 +53,7 @@ router.all('/api/sql/:db', function (req, response) {
             'X-Powered-By': 'MapCentia Vidi'
         }
     } else {
+        fileName = key + ".json";
         headers = {
             'Content-Type': 'application/json',
             'Expires': '0',
@@ -54,15 +62,30 @@ router.all('/api/sql/:db', function (req, response) {
         }
     }
 
-    response.writeHead(200, headers);
+    if (!store) {
+        response.writeHead(200, headers);
+    }
 
-    var rem = request(options);
+    rem = request(options);
+
+    if (store) {
+        writeStream = fs.createWriteStream(__dirname + "/../../public/tmp/stored_results/" + fileName);
+    }
 
     rem.on('data', function (chunk) {
-        response.write(chunk);
+        if (store) {
+            writeStream.write(chunk, 'binary');
+        } else {
+            response.write(chunk);
+        }
     });
     rem.on('end', function () {
-        response.end();
+        if (store) {
+            console.log("Result saved");
+            response.send({"success": true, "file": fileName});
+        } else {
+            response.end();
+        }
     });
 
 });
