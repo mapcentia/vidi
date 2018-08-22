07ed3b0c08e6dd7cef5be9d789b48677b50d87e1
sashuk <shumsan1011@gmail.com>
2018-08-22 03:57:57 +0000

    Temporary changes for third-level layer subgroups

--- browser/modules/layerTree.js
+++ browser/modules/layerTree.js
@@ -484,6 +484,10 @@ module.exports = {
                 $("#layers").empty();
 
                 _self.getLayersOrder().then(order => {
+
+                    try {
+
+
                     let activeLayers = [];
                     if (forcedState) {
                         order = forcedState.order;
@@ -542,32 +546,11 @@ module.exports = {
                     }
 
                     let notSortedGroupsArray = array_unique(groups.reverse());
-                    let arr = [];
                     metaData.data.reverse();
 
+                    let arr = notSortedGroupsArray;
                     if (order) {
-                        for (let key in order) {
-                            let item = order[key];
-                            let sortedElement = false;
-                            for (let i = (notSortedGroupsArray.length - 1); i >= 0; i--) {
-                                if (item.id === notSortedGroupsArray[i]) {
-                                    sortedElement = notSortedGroupsArray.splice(i, 1);
-                                    break;
-                                }
-                            }
-
-                            if (sortedElement) {
-                                arr.push(item.id);
-                            }
-                        }
-
-                        if (notSortedGroupsArray.length > 0) {
-                            for (let j = 0; j < notSortedGroupsArray.length; j++) {
-                                arr.push(notSortedGroupsArray[j]);
-                            }
-                        }
-                    } else {
-                        arr = notSortedGroupsArray;
+                        arr = _self.sortGroups(order, notSortedGroupsArray);
                     }
 
                     $("#layers").append(`<div id="layers_list"></div>`);
@@ -607,47 +590,17 @@ module.exports = {
                                 }
                             }
 
-                            // Sort layers if there is available order
-                            let layersForCurrentGroup = false;
-                            let sortedLayers = [];
-                            if (order) {
-                                let currentGroupLayersOrder = false;
-                                for (let key in order) {
-                                    if (order[key].id === arr[i] && 'layers' in order[key]) {
-                                        currentGroupLayersOrder = order[key].layers;
-                                    }
-                                }
+                            let layersForCurrentGroup = _self.sortLayers(order, notSortedLayersForCurrentGroup, arr[i]);
 
-                                if (currentGroupLayersOrder) {
-                                    for (let key in currentGroupLayersOrder) {
-                                        let item = currentGroupLayersOrder[key];
-                                        let sortedElement = false;
-                                        for (let i = (notSortedLayersForCurrentGroup.length - 1); i >= 0; i--) {
-                                            let layerId = notSortedLayersForCurrentGroup[i].f_table_schema + '.' + notSortedLayersForCurrentGroup[i].f_table_name;
-                                            if (item.id === layerId) {
-                                                sortedElement = notSortedLayersForCurrentGroup.splice(i, 1);
-                                                break;
-                                            }
-                                        }
 
-                                        if (sortedElement) {
-                                            sortedLayers.push(sortedElement.pop());
-                                        }
-                                    }
 
-                                    if (notSortedLayersForCurrentGroup.length > 0) {
-                                        for (let j = 0; j < notSortedLayersForCurrentGroup.length; j++) {
-                                            sortedLayers.push(notSortedLayersForCurrentGroup[j]);
-                                        }
-                                    }
-                                }
-                            }
 
-                            if (sortedLayers.length > 0) {
-                                layersForCurrentGroup = sortedLayers;
-                            } else {
-                                layersForCurrentGroup = notSortedLayersForCurrentGroup;
-                            }
+
+                            // @todo Here go subgroups
+                            console.log(`### layersForCurrentGroup`, layersForCurrentGroup);
+
+
+
 
                             // Add layers
                             // ==========
@@ -936,11 +889,105 @@ module.exports = {
 
                         resolve();
                     }, 1000);
+
+
+                }catch(e) {
+                    console.log(e);
+                }
+
+
                 });
             });
         }
 
         return result;
+    },
+
+
+    /**
+     * Sorts groups according to defined order (not all groups
+     * can be present in order definition).
+     * 
+     * @returns {Array}
+     */
+    sortGroups: (order, notSortedGroupsArray) => {
+        let result = [];
+        for (let key in order) {
+            let item = order[key];
+            let sortedElement = false;
+            for (let i = (notSortedGroupsArray.length - 1); i >= 0; i--) {
+                if (item.id === notSortedGroupsArray[i]) {
+                    sortedElement = notSortedGroupsArray.splice(i, 1);
+                    break;
+                }
+            }
+
+            if (sortedElement) {
+                result.push(item.id);
+            }
+        }
+
+        if (notSortedGroupsArray.length > 0) {
+            for (let j = 0; j < notSortedGroupsArray.length; j++) {
+                result.push(notSortedGroupsArray[j]);
+            }
+        }
+
+        return result;
+    },
+
+    /**
+     * Sorts layers and their subgroups
+     * 
+     * @returns {Array}
+     */
+    sortLayers: (order, notSortedLayersForCurrentGroup, groupName) => {
+        let sortedLayers = [];
+        if (order) {
+            let currentGroupLayersOrder = false;
+            for (let key in order) {
+                if (order[key].id === groupName && 'layers' in order[key]) {
+                    currentGroupLayersOrder = order[key].layers;
+                }
+            }
+
+            if (currentGroupLayersOrder) {
+                for (let key in currentGroupLayersOrder) {
+                    let item = currentGroupLayersOrder[key];
+                    let sortedElement = false;
+                    for (let i = (notSortedLayersForCurrentGroup.length - 1); i >= 0; i--) {
+                        let layerId = notSortedLayersForCurrentGroup[i].f_table_schema + '.' + notSortedLayersForCurrentGroup[i].f_table_name;
+                        if (item.id === layerId) {
+                            sortedElement = notSortedLayersForCurrentGroup.splice(i, 1);
+                            break;
+                        }
+                    }
+
+                    if (sortedElement) {
+                        sortedLayers.push(sortedElement.pop());
+                    }
+                }
+
+                if (notSortedLayersForCurrentGroup.length > 0) {
+                    for (let j = 0; j < notSortedLayersForCurrentGroup.length; j++) {
+                        sortedLayers.push(notSortedLayersForCurrentGroup[j]);
+                    }
+                }
+            }
+        }
+
+        /*
+            There is a list of layers, some of them belong to subgroups, some of them do not
+            First go to order object and find what subgroups are there, remember theirs order and order of underlying layers
+            After that get all subgroups from existing layers, and match the, to the ordered ones - those that are not in order, put them afterwards
+            Match meta layers of every subgroup and the order in subgroup - the meta data is more important in terms of belonging than the order
+
+            1. Order and complete subgroups
+            2. Order and complete layers inside subgroups
+            3. Store as three-level hierarchy
+        */
+
+        return ((sortedLayers.length > 0) ? sortedLayers : notSortedLayersForCurrentGroup);
     },
 
     calculateOrder: () => {
