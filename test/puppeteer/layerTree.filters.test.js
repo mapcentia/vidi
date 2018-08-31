6f71fc702bc4f9eef6ea7447bf127b7ad23420f0
sashuk <shumsan1011@gmail.com>
2018-08-31 06:31:13 +0000

    Filter testing was implemented

--- test/puppeteer/layerTree.filters.test.js
+++ test/puppeteer/layerTree.filters.test.js
@@ -5,9 +5,11 @@
 const { expect } = require("chai");
 const helpers = require("./../helpers");
 
+const PAGE_URL = `https://vidi.alexshumilov.ru/app/aleksandrshumilov/test/#stamenTonerLite/13/39.2681/-6.8108/v:test.testpointfilters`;
+
 const createPage = async () => {
     const page = await browser.newPage();
-    await page.goto(`https://vidi.alexshumilov.ru/app/aleksandrshumilov/test/#stamenTonerLite/13/39.2681/-6.8108/v:test.testpointfilters`);
+    await page.goto(PAGE_URL);
     await helpers.sleep(helpers.PAGE_LOAD_TIMEOUT);
     await page._client.send('Network.enable');
 
@@ -67,9 +69,87 @@ target.dispatchEvent(event);`);
 };
 
 describe('Layer tree filters', () => {
-    it('should store its state', async () => { });
-    it('should allow adding and deleting rules', async () => { });
-    it('should allow AND and OR modes of filtering', async () => { });
+    it('should store filters in state snapshot', async () => {
+        let page = await createPage();
+
+        let numberOfFilteredItems = false;
+        page.on(`response`, async response => {
+            if (response.url().indexOf(`/api/sql/aleksandrshumilov`) !== -1) {
+                let parsedResponse = await response.json();
+                numberOfFilteredItems = parsedResponse.features.length;
+            }
+        });
+
+        await setTextFilterValue(page, `stringfield`, `like`, `abc`, 0, false);
+        expect(await page.evaluate(`$('[id="layer-settings-filters-test.testpointfilters"').find('[class="btn btn-sm"]').first().trigger('click')`));
+        await setTextFilterValue(page, `decimalfield`, `=`, `1.4`, 1);
+
+        expect(numberOfFilteredItems).to.equal(4);
+
+        await page.click(`[href="#state-snapshots-dialog-content-content"]`);   
+        await helpers.sleep(1000);
+
+        // Create state snapshot
+        await page.type(`.js-browser-owned input`, `test snapshot title`);
+        await helpers.sleep(2000);
+        await page.evaluate(`$('.js-browser-owned').find('button').first().trigger('click')`);
+        await helpers.sleep(3000);
+
+        // Reload page
+        const newPage = await browser.newPage();
+        await newPage.goto(PAGE_URL);
+        newPage.on(`response`, async response => {
+            if (response.url().indexOf(`/api/sql/aleksandrshumilov`) !== -1) {
+                let parsedResponse = await response.json();
+                numberOfFilteredItems = parsedResponse.features.length;
+            }
+        });
+
+        await newPage.emulate(helpers.EMULATED_SCREEN);
+        await helpers.sleep(helpers.PAGE_LOAD_TIMEOUT);
+        expect(numberOfFilteredItems).to.equal(7);
+
+        // Restore snapshot
+        await newPage.evaluate(`$('#search-border').trigger('click')`);
+        await helpers.sleep(500);
+        await newPage.click(`[href="#state-snapshots-dialog-content-content"]`);   
+        await helpers.sleep(1000);
+
+        await newPage.evaluate(`$('#state-snapshots-dialog-content').find('.panel-default').eq(0).find('button').first().trigger('click')`);
+        await helpers.sleep(2000);
+        expect(numberOfFilteredItems).to.equal(4);
+    });
+
+    it('should allow AND / OR modes of filtering, as well as adding and deleting of rules', async () => {
+        let page = await createPage();
+
+        let numberOfFilteredItems = false;
+        page.on(`response`, async response => {
+            if (response.url().indexOf(`/api/sql/aleksandrshumilov`) !== -1) {
+                let parsedResponse = await response.json();
+                numberOfFilteredItems = parsedResponse.features.length;
+            }
+        });
+
+        await setTextFilterValue(page, `stringfield`, `like`, `abc`, 0, false);
+        expect(await page.evaluate(`$('[id="layer-settings-filters-test.testpointfilters"').find('[class="btn btn-sm"]').first().trigger('click')`));
+        await setTextFilterValue(page, `decimalfield`, `=`, `1.4`, 1);
+
+        expect(numberOfFilteredItems).to.equal(4);
+        
+        await page.select(`select[id="match_select_testpointfilters.test"]`, `all`);
+        await helpers.sleep(500);
+        await page.evaluate(`$('[id="layer-settings-filters-test.testpointfilters"').find('[class="btn btn-sm btn-success"]').trigger('click')`);
+        await helpers.sleep(2000);
+
+        expect(numberOfFilteredItems).to.equal(1);
+
+        await page.evaluate(`$('[id="layer-settings-filters-test.testpointfilters"').find('[class="btn btn-xs btn-warning"]').first().trigger('click')`);
+        await page.evaluate(`$('[id="layer-settings-filters-test.testpointfilters"').find('[class="btn btn-sm btn-success"]').trigger('click')`);
+        await helpers.sleep(2000);
+
+        expect(numberOfFilteredItems).to.equal(2);
+    });
 
     it('should enable Apply button accordingly to rules validity', async () => {
         let page = await createPage();
