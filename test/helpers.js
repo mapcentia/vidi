2d59ace49b6ee149500ef7b2b559f39efd1f8cf4
sashuk <shumsan1011@gmail.com>
2018-09-25 10:27:55 +0000

    Logging was reverted

--- test/helpers.js
+++ test/helpers.js
@@ -9,7 +9,6 @@ module.exports = {
     // @todo Independent development server has to be deployed in order to handle e2e tests
     API_URL: `https://vidi.alexshumilov.ru:8081/api`,
     PAGE_URL: `https://vidi.alexshumilov.ru:8082/app/aleksandrshumilov/public/#osm/13/39.2963/-6.8335/`,
-    //PAGE_URL: `https://vidi.alexshumilov.ru/app/aleksandrshumilov/public/#osm/13/39.2963/-6.8335/`,
     PAGE_URL_BASE: `https://vidi.alexshumilov.ru:8082/`,
     PAGE_LOAD_TIMEOUT: 1000,
     EMULATED_SCREEN: {
@@ -24,7 +23,7 @@ module.exports = {
     waitForPageToLoad: async (page) => {
         let loadedPage = new Promise((resolve, reject) => {
             page.on('console', async (msg) => {
-                console.log(msg.text());
+                //console.log(msg.text());
                 if (msg.text().indexOf(`Vidi is now loaded`) !== -1) {
                     await sleepFunction(1000);
                     resolve(page);
