91c830e9c8bd0c7faa10ced2cb7a04450011d2b8
Martin Høgh <mh@mapcentia.com>
2018-10-29 14:01:20 +0100

    Refactoring

--- public/js/vidi.js
+++ public/js/vidi.js
@@ -44,17 +44,9 @@ function touchScroll(selector) {
 }
 
 function array_unique(ar) {
-    if (ar.length && typeof ar !== 'string') {
-        var sorter = {};
-        var out = [];
-        for (var i = 0, j = ar.length; i < j; i++) {
-            if (!sorter[ar[i] + typeof ar[i]]) {
-                out.push(ar[i]);
-                sorter[ar[i] + typeof ar[i]] = true;
-            }
-        }
-    }
-    return out || ar;
+    return ar.filter( function onlyUnique(value, index, self) {
+        return self.indexOf(value) === index;
+    } )
 }
 
 /**
