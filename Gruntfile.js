3803b4368cd13e32ea310669200797c8e03cf739
Martin Høgh <mh@mapcentia.com>
2018-11-28 14:08:56 +0100

    Remove inline css

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,9 @@
 
 module.exports = function (grunt) {
     "use strict";
+
+    const posthtmlInlineAssets = require('posthtml-inline-assets');
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         env: {
@@ -311,12 +314,6 @@ module.exports = function (grunt) {
                     verbose: true,
                 }
             }
-        },
-
-        inline: {
-            dist: {
-                src: './public/index.html'
-            }
         }
     });
 
@@ -354,7 +351,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-watchify');
     grunt.loadNpmTasks('grunt-version');
-    grunt.loadNpmTasks('grunt-inline');
 
     grunt.registerTask('default', ['browserify:publish', 'browserify:publish_sw_dev', 'extension-css', 'hogan', 'version']);
     grunt.registerTask('production', ['env', 'gitreset', 'hogan', 'browserify:publish', 'browserify:publish_sw', 'extension-css', 'shell', 'uglify', 'processhtml', 'cssmin:build', 'cacheBust', 'inline', 'version', 'appendBuildHashToVersion']);
