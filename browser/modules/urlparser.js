85912e2fb5d097110ae563ee4f575d5327bf651f
Martin Høgh <mh@mapcentia.com>
2019-02-27 19:39:27 +0100

    Hostname set from URL, not geocloud_host global var

--- browser/modules/urlparser.js
+++ browser/modules/urlparser.js
@@ -1,23 +1,21 @@
 /*
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2018 MapCentia ApS
+ * @copyright  2013-2019 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  */
 
 'use strict';
 
-var uriJs = require('urijs');
-
-var uriObj = new uriJs(window.location.href);
-
-var queryStr = uriObj.search();
+let uriJs = require('urijs');
+let uriObj = new uriJs(window.location.href);
+let queryStr = uriObj.search();
 
 /**
  *
  * @type {{hostname: *, hash: string, db: *, schema: *, urlVars: *}}
  */
 module.exports = {
-    hostname: geocloud_host,
+    hostname: uriObj.protocol() + "://" + uriObj.hostname() + ":" + uriObj.port(),
     hash: decodeURIComponent(geocloud.urlHash),
     db: uriObj.segmentCoded(1),
     schema: uriObj.segmentCoded(2),
