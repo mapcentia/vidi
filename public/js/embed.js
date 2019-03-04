3ea046cae11a0a433d2b63e88b2609e89ba4128b
Martin Høgh <mh@mapcentia.com>
2019-03-04 11:42:26 +0100

    Use token instead of state id

--- public/js/embed.js
+++ public/js/embed.js
@@ -5,44 +5,41 @@
  */
 
 (function () {
+
     /**
      * Creates the iFrame in targetDiv DOM element
      * @param targetDiv
      */
     var create = function (targetDiv) {
-        var id = targetDiv.attributes["data-vidi-snapshot"].value;
+        var token = targetDiv.attributes["data-vidi-token"].value;
         var width = targetDiv.getAttribute("data-vidi-width") || "500px";
         var height = targetDiv.getAttribute("data-vidi-height") || "500px";
         var temp = targetDiv.getAttribute("data-vidi-temp") || "embed.tmpl";
-        var host = targetDiv.getAttribute("data-vidi-host");
-        var db = targetDiv.getAttribute("data-vidi-db");
-
-        if (!host || !db) {
-            alert("No host or db set");
-            return false;
+        try {
+            var obj = JSON.parse(atob(token));
+        } catch (e) {
+            alert("Could not parse token");
         }
-
+        var host = obj.host + ""; // Port ?
+        var id = obj.id;
+        var database = obj.database;
         var iframe = document.createElement("iframe");
         iframe.setAttribute("style", "width:" + width + ";height:" + height + ";border: 1px solid rgba(0,0,0,0.1)");
         iframe.setAttribute("allowfullscreen", "");
-        iframe.setAttribute("src", host + "/app/" + db + "/?state=" + id + "&tmpl=" + temp);
+        iframe.setAttribute("src", host + "/app/" + database + "/?state=" + id + "&tmpl=" + temp);
         targetDiv.append(iframe);
     };
-
     // If script is loaded at bottom of page, when select above elements
-    document.querySelectorAll('[data-vidi-snapshot]').forEach(function (object) {
-        if (object.attributes && object.attributes["data-vidi-snapshot"] && object.attributes["data-vidi-snapshot"] !== null && object.attributes["data-vidi-snapshot"].value !== undefined) {
+    document.querySelectorAll('[data-vidi-token]').forEach(function (object) {
+        if (object.attributes && object.attributes["data-vidi-token"] && object.attributes["data-vidi-token"] !== null && object.attributes["data-vidi-token"].value !== undefined) {
             create(object);
         }
-
     });
-
     // If script is loaded at top of page, when use MutationObserver to detect arriving elements
     new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
             mutation.addedNodes.forEach(function (object) {
-
-                if (object.attributes && object.attributes["data-vidi-snapshot"] && object.attributes["data-vidi-snapshot"] !== null && object.attributes["data-vidi-snapshot"].value !== undefined) {
+                if (object.attributes && object.attributes["data-vidi-token"] && object.attributes["data-vidi-token"] !== null && object.attributes["data-vidi-token"].value !== undefined) {
                     create(object);
                 }
 
@@ -50,5 +47,3 @@
         });
     }).observe(document, {childList: true, subtree: true});
 }());
-
-
