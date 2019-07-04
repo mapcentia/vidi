249daa06a03eca011dd52c5d8878e2d268f802eb
Martin Høgh <mh@mapcentia.com>
2019-07-04 21:36:11 +0200

    set undefined key to text string

--- controllers/gc2/sql.js
+++ controllers/gc2/sql.js
@@ -32,7 +32,7 @@ router.all('/api/sql/:db', function (req, response) {
             return v.toString(16);
         });
 
-    var postData = "q=" + (base64 === "true" ? encodeURIComponent(q) : encodeURIComponent(q)) + "&base64=" + (base64 === "true" ? "true" : "false") + "&srs=" + srs + "&lifetime=" + lifetime + "&client_encoding=" + (client_encoding || "UTF8") + "&format=" + (format ? format : "geojson") + "&key=" + req.session.gc2ApiKey + "&custom_data=" + (custom_data || ""),
+    var postData = "q=" + (base64 === "true" ? encodeURIComponent(q) : encodeURIComponent(q)) + "&base64=" + (base64 === "true" ? "true" : "false") + "&srs=" + srs + "&lifetime=" + lifetime + "&client_encoding=" + (client_encoding || "UTF8") + "&format=" + (format ? format : "geojson") + "&key=" + (typeof req.session.gc2ApiKey !=="undefined" ? req.session.gc2ApiKey : "xxxxx") + "&custom_data=" + (custom_data || ""),
         options;
 
     // Check if user is a sub user
