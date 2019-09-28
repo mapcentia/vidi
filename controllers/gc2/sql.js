9d23c4380af0be9685ec15dc714c68fb884abb26
Martin Høgh <mh@mapcentia.com>
2019-09-29 01:11:07 +0200

    Dummy key

--- controllers/gc2/sql.js
+++ controllers/gc2/sql.js
@@ -23,7 +23,7 @@ router.all('/api/sql/:db', function (req, response) {
             return v.toString(16);
         });
 
-    var postData = "q=" + encodeURIComponent(q) + "&base64=" + (base64 === "true" ? "true" : "false") + "&srs=" + srs + "&lifetime=" + lifetime + "&client_encoding=" + client_encoding + "&format=" + (format ? format : "geojson") + "&key=" + req.session.gc2ApiKey,
+    var postData = "q=" + encodeURIComponent(q) + "&base64=" + (base64 === "true" ? "true" : "false") + "&srs=" + srs + "&lifetime=" + lifetime + "&client_encoding=" + client_encoding + "&format=" + (format ? format : "geojson") + "&key=" + (typeof req.session.gc2ApiKey !=="undefined" ? req.session.gc2ApiKey : "xxxxx" /*Dummy key is sent to prevent start of session*/);
         options;
 
     // Check if user is a sub user
