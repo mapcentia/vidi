6565bc03a46471549c089aaa2d2e099d9b032c61
Martin Høgh <mh@mapcentia.com>
2020-05-27 11:14:31 +0200

    Left slide panel for default.tmpl and all args in info_function when doing feature info on tile layers

--- browser/modules/sqlQuery.js
+++ browser/modules/sqlQuery.js
@@ -349,7 +349,7 @@ module.exports = {
                         if (typeof parsedMeta.info_function !== "undefined" && parsedMeta.info_function !== "") {
                             try {
                                 let func = Function('"use strict";return (' + parsedMeta.info_function + ')')();
-                                func(null, null, null, _self, null, cloud.get().map);
+                                func(this.layer.toGeoJSON(), this.layer, keyWithoutGeom, _self, this, cloud.get().map);
                             } catch (e) {
                                 console.info("Error in click function for: " + _key_);
                                 console.error(e.message);
