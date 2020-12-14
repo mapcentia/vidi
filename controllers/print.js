90ebe97a11010a23ba5fe21d94302e075fe8d5a1
Lars Klindt <lkm@geopartner.dk>
2020-12-14 13:23:23 +0100

    Inc print timeout

--- controllers/print.js
+++ controllers/print.js
@@ -133,7 +133,7 @@ function print(key, q, req, response, outputPng = false, frame = 0, count, retur
                             headless.destroy(browser);
                             console.log("Destroying browser after 60 secs");
                         }
-                    }, 60000);
+                    }, 120000);
                     if (!outputPng) {
                         browser.newPage().then(async (page) => {
                             await page.emulateMedia('screen');
