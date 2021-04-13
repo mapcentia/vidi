fa08c969637c772b52cf54060a9fdfc8f7700d14
Martin Høgh <mh@mapcentia.com>
2021-04-13 16:48:34 +0200

    Limit number of edit fields

--- extensions/editor/browser/index.js
+++ extensions/editor/browser/index.js
@@ -267,9 +267,9 @@ module.exports = {
         let uiSchema = {};
 
         Object.keys(fields).map(function (key) {
-            if (key !== pkey && key !== f_geometry_column && (key.indexOf(SYSTEM_FIELD_PREFIX) !== 0 || (typeof fieldConf[key] !== "undefined" && fieldConf[key].querable === true))) {
+            if (key !== pkey && key !== f_geometry_column && (key.indexOf(SYSTEM_FIELD_PREFIX) !== 0 && !fieldConf[key]?.filter)) {
                 let title = key;
-                if (fieldConf[key] !== undefined && fieldConf[key].alias) {
+                if (fieldConf[key]?.alias) {
                     title = fieldConf[key].alias;
                 }
 
