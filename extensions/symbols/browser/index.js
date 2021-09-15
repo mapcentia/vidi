caf9c94a771e08379111488facdad450c1fad908
Martin Høgh <mh@mapcentia.com>
2021-09-15 12:21:51 +0200

    State and snapshots working for symbols

--- extensions/symbols/browser/index.js
+++ extensions/symbols/browser/index.js
@@ -21,8 +21,12 @@ let _self;
 const MODULE_ID = exId;
 let mouseDown = false;
 let touch = false;
+let idBeingChanged = false;
+let creatingFromState = false;
 
 const store = (id) => {
+    // TODO Store symbol in database
+    console.log(id);
     backboneEvents.get().trigger(`${MODULE_ID}:state_change`);
 }
 
@@ -43,6 +47,7 @@ const rotate = (e, img, id, classStr) => {
         let radians = Math.atan2(mouseX - centerX, mouseY - centerY);
         symbolState[id].rotation = (radians * (180 / Math.PI) * -1) + 135;
         img.css('transform', 'rotate(' + symbolState[id].rotation.toString() + 'deg) scale(' + symbolState[id].scale.toString() + ')');
+        idBeingChanged = id;
     }
 }
 const scale = (e, img, id, classStr) => {
@@ -64,6 +69,7 @@ const scale = (e, img, id, classStr) => {
         let c = Math.sqrt(a * a + b * b);
         symbolState[id].scale = (c / 40);
         img.css('transform', 'rotate(' + symbolState[id].rotation.toString() + 'deg) scale(' + symbolState[id].scale.toString() + ')');
+        idBeingChanged = id;
     }
 }
 
@@ -96,8 +102,9 @@ const createSymbol = (innerHtml, id, coord, ro = 0, sc = 1) => {
     let rotateHandleStr = "r_" + id;
     let deleteHandleStr = "d_" + id;
     let scaleHandleStr = "s_" + id;
-    let outerHtml = $(symbolWrapper).html(innerHtml);
+    let outerHtml = $(symbolWrapper).clone().html(innerHtml);
     outerHtml.removeClass("symbols-lib");
+    outerHtml.removeClass("col");
     outerHtml.addClass("symbols-map");
     outerHtml.attr("draggable", "false")
     outerHtml.addClass(classStr);
@@ -116,12 +123,14 @@ const createSymbol = (innerHtml, id, coord, ro = 0, sc = 1) => {
     symbolState[id].coord = markers[id].getLatLng();
     markers[id].on("moveend", () => {
         symbolState[id].coord = markers[id].getLatLng();
-        store(id);
+        idBeingChanged = id;
     })
     symbolState[id].rotation = ro;
     symbolState[id].scale = sc;
 
-    store(id);
+    if (!creatingFromState) {
+        store(id);
+    }
     let img = $(`.${classStr}`);
     let rotateHandle = $(`.${rotateHandleStr}`);
     let deleteHandle = $(`.${deleteHandleStr}`);
@@ -204,97 +213,135 @@ module.exports = {
             _self.applyState(initialState)
         });
 
+        backboneEvents.get().on(`reset:all`, () => {
+            // alert()
+            _self.resetState();
+        });
+
+        backboneEvents.get().on(`off:all`, () => {
+            // _self.control(false);
+            // _self.off();
+        });
+
         utils.createMainTab(exId, __("Symboles"), __("Info"), require('./../../../browser/modules/height')().max, "photo_camera", false, exId);
-        $(`#${exId}`).append(dom);
+
+        let outer = $(htmlFragments.outer);
+        let inner = $(htmlFragments.inner);
+
+        for (const id in symbols) {
+            let svg = $(inner.clone()[0]).append(symbols[id])
+            outer.find('.row')[0].append(svg[0])
+        }
+
+        $(`#${exId}`).append(outer);
 
         $(".drag-marker").on("dragend", handleDragEnd)
 
-        let markerElement = $($(".symbols-lib")[1]).clone().html();
-
-
-        $(document).on("touchend mouseup", function (e) {
+        $(document).on("touchend mouseup", function () {
             for (const id in symbolState) {
                 markers[id].dragging.enable();
             }
             map.dragging.enable();
-            map.touchZoom.enable()
+            map.touchZoom.enable();
+            if (idBeingChanged) {
+                store(idBeingChanged);
+            }
             $(document).off("mousemove touchmove");
             mouseDown = false;
+            idBeingChanged = false;
         });
     },
-    recreateSymbolsFromState: (state)=>{
+    recreateSymbolsFromState: (state) => {
+        creatingFromState = true;
         for (const id in state) {
-            createSymbol(state[id].svg, id, state[id].coord, state[id].rotation, state[id].scale);
+            if (id && state.hasOwnProperty(id)) {
+                createSymbol(state[id].svg, id, state[id].coord, state[id].rotation, state[id].scale);
+            }
         }
+        creatingFromState = false;
     },
 
     getState: () => {
-        console.log(symbolState)
         return symbolState;
     },
 
     applyState: (newState) => {
-        console.log("newState", newState)
         return new Promise((resolve) => {
-            _self.recreateSymbolsFromState(newState);
+            _self.resetState();
+            if (newState) {
+                _self.recreateSymbolsFromState(newState);
+            }
+            backboneEvents.get().trigger(`${MODULE_ID}:state_change`);
             resolve();
+
         });
+    },
+
+    resetState: () => {
+        mouseDown = false;
+        touch = false;
+        idBeingChanged = false;
+        creatingFromState = false;
+        _self.removeSymbols();
+    },
+
+    removeSymbols: () => {
+        let map = cloud.get().map;
+        for (const id in symbolState) {
+            map.removeLayer(markers[id]);
+        }
+        markers = {};
+        symbolState = {};
     }
 };
 
-let dom = `
-
-<div>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="symbols-lib drag-marker" draggable="true">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>
-                    curved-arrow-down-glyph</title>
-                    <path d="M492.93,170.85C492.93,76.64,416.29,0,322.09,0S151.25,76.64,151.25,170.85l-.12,184.77L80.2,284.17a9.83,9.83,0,0,0-13.9,0L21.9,328.57a9.84,9.84,0,0,0,0,13.9L188.54,509.11a9.81,9.81,0,0,0,13.9,0L369.08,342.48a9.83,9.83,0,0,0,0-13.9l-44.4-44.41a10.11,10.11,0,0,0-13.9,0L239.6,355.62l.13-184.77a82.35,82.35,0,1,1,164.7,0l0,159.84a9.94,9.94,0,0,0,9.93,9.93H483a9.94,9.94,0,0,0,9.93-9.93Z"/>
-                </svg>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="symbols-lib drag-marker" draggable="true">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>
-                    arrow-right-down-glyph</title>
+const symbols = {
+    s1: `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M492.93,170.85C492.93,76.64,416.29,0,322.09,0S151.25,76.64,151.25,170.85l-.12,184.77L80.2,284.17a9.83,9.83,0,0,0-13.9,0L21.9,328.57a9.84,9.84,0,0,0,0,13.9L188.54,509.11a9.81,9.81,0,0,0,13.9,0L369.08,342.48a9.83,9.83,0,0,0,0-13.9l-44.4-44.41a10.11,10.11,0,0,0-13.9,0L239.6,355.62l.13-184.77a82.35,82.35,0,1,1,164.7,0l0,159.84a9.94,9.94,0,0,0,9.93,9.93H483a9.94,9.94,0,0,0,9.93-9.93Z"/>
+            </svg>
+    `,
+    s2: `
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M487.31,275.08a11.1,11.1,0,0,0-9.61-5.55H421.54V11.1A11.1,11.1,0,0,0,410.44,0H120a11.11,11.11,0,0,0-9,4.64L25.27,124.51a11.1,11.1,0,0,0,9,17.55H279.47V269.53H223.3a11.1,11.1,0,0,0-9.61,16.65L340.9,506.45a11.1,11.1,0,0,0,19.22,0L487.3,286.18A11.09,11.09,0,0,0,487.31,275.08Z"/>
                 </svg>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="symbols-lib drag-marker" draggable="true">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>
-                    arrow-curved-top-glyph</title>
+
+    `,
+    s3: `
+    
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M376.06,238.67v104a168.69,168.69,0,0,1-49.83,119.51v.36A169.16,169.16,0,0,1,37.37,342.66V136.12h81.24V342.66a87.53,87.53,0,0,0,88.1,88.1,88.14,88.14,0,0,0,88.1-88.1v-104H199.13l69-119.51L337.06,0l68.6,119.15,69,119.51Z"
                           fill="#434040"/>
                 </svg>
-            </div>
-        </div>
-    </div>
-
-    <div class="row" style="margin-top: 25px">
-        <div class="col-md-4">
-            <div class="symbols-lib drag-marker" draggable="true">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>
-                    arrow-left-glyph</title>
+    
+    `,
+    s4: `
+    
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M501.34,62.07h-63a10.66,10.66,0,0,0-10.66,10.66V297.52H193.73V240.13a10.66,10.66,0,0,0-16-9.23L5.33,330.47a10.66,10.66,0,0,0,0,18.46L177.75,448.5a10.66,10.66,0,0,0,16-9.23V381.88H501.34A10.66,10.66,0,0,0,512,371.22V72.73A10.66,10.66,0,0,0,501.34,62.07Z"/>
                 </svg>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="symbols-lib drag-marker" draggable="true">
-                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><title>
-                    arrow-top-glyph</title>
+    
+    `,
+    s5: `
+    
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <polygon
                             points="256 0 362.35 115.09 468.7 230.55 338.04 230.55 338.04 512 166.74 512 166.74 230.55 43.3 230.55 149.65 115.09 256 0"
                             fill="#434040"/>
                 </svg>
+    
+    `
+}
+
+const htmlFragments = {
+    "outer": `
+        <div class="container">
+            <div class="row row-cols-2">
             </div>
         </div>
-        <div class="col-md-4">
-        </div>
-    </div>
-</div>
-`
+    `,
+    "inner": `
+            <div class="col symbols-lib drag-marker" draggable="true"></div>
+    `
+}
 
