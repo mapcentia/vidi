330ab514f03c192c3d4065a38f12aeba7f02bd45
Rene Giovanni Borella <rgb@mapster.dk>
2021-12-07 21:00:49 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -866,6 +866,28 @@ module.exports = {
                     }
                 }
             },
+            "vejbysmidstrup_print": { // Your print templates. "print" is the default
+                A4: {
+                    l: {
+                        mapsizePx: [1060, 730],
+                        mapsizeMm: [280, 192]
+                    },
+                    p: {
+                        mapsizePx: [730, 1060],
+                        mapsizeMm: [192, 280]
+                    }
+                },
+                A3: {
+                    l: {
+                        mapsizePx: [1525, 1065],
+                        mapsizeMm: [401, 282]
+                    },
+                    p: {
+                        mapsizePx: [1065, 1525],
+                        mapsizeMm: [282, 401]
+                    }
+                }
+            },
 
             // ========================================================
             // Names starting with "_" will not appear in the Print tab
