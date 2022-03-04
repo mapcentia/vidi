335c428b710a0898099b7480219f41d15cd7f8b1
Morten Møllebæk <mmo@geopartner.dk>
2022-03-04 11:09:04 +0100

    Andels.net
    Printopsætning til Andels.net

--- config/config.js
+++ config/config.js
@@ -53,6 +53,28 @@ module.exports = {
             // The "print" template is build-in
             // ================================
 
+            "andelsnet": { // Your print templates. "print" is the default
+                A4: {
+                    l: {
+                        mapsizePx: [1060, 730],
+                        mapsizeMm: [280, 192]
+                    },
+                    p: {
+                        mapsizePx: [730, 1060],
+                        mapsizeMm: [192, 280]
+                    }
+                },
+                A3: {
+                    l: {
+                        mapsizePx: [1525, 1065],
+                        mapsizeMm: [401, 282]
+                    },
+                    p: {
+                        mapsizePx: [1065, 1525],
+                        mapsizeMm: [282, 401]
+                    }
+                }
+            },
             "vandvaerk_sjoerupvand2": { // Your print templates. "print" is the default
                 A4: {
                     l: {
