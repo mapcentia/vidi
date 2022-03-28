afe7a395df3ec63ab58bb5d7914351049fdb9107
Lars Lindenborg Jensen <59565771+larslindenborg@users.noreply.github.com>
2022-03-28 12:58:21 +0200

    tilføjet skagen havn

--- config/config.js
+++ config/config.js
@@ -844,6 +844,28 @@ module.exports = {
                     }
                 }
             },
+			"skagenhavn_print": { // Your print templates. "print" is the default
+                A4: {
+                    l: {
+                        mapsizePx: [1060, 730],
+                        mapsizeMm: [280, 192]
+                    },
+                    p: {
+                        mapsizePx: [730, 1060],
+                        mapsizeMm: [192, 280]
+                    }
+                },
+                A3: {
+                    l: {
+                        mapsizePx: [1525, 1065],
+                        mapsizeMm: [401, 282]
+                    },
+                    p: {
+                        mapsizePx: [1065, 1525],
+                        mapsizeMm: [282, 401]
+                    }
+                }
+            },
             "hoejslevkirkeby_print": { // Your print templates. "print" is the default
                 A4: {
                     l: {
