0fdd2d78fff662080ab378f120c9fa03577438bd
Lars Klindt <lkm@geopartner.dk>
2022-05-18 17:08:40 +0200

    Added seges print template

--- config/config.js
+++ config/config.js
@@ -377,6 +377,28 @@ module.exports = {
                     }
                 }
             },
+            "segesprint": { // Your print templates. "print" is the default
+                A4: {
+                    l: {
+                        mapsizePx: [1060, 730],
+                        mapsizeMm: [280, 192]
+                    },
+                    p: {
+                        mapsizePx: [730, 1060],
+                        mapsizeMm: [192, 280]
+                    }
+                },
+                A3: {
+                    l: {
+                        mapsizePx: [1525, 1065],
+                        mapsizeMm: [401, 282]
+                    },
+                    p: {
+                        mapsizePx: [1065, 1525],
+                        mapsizeMm: [282, 401]
+                    }
+                }
+            },
             "grundfosprint": { // Your print templates. "print" is the default
                 A4: {
                     l: {
