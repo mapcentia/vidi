9bec96c63e4e21177cadbe8840966d11c5fd8771
Lars Lindenborg Jensen <59565771+larslindenborg@users.noreply.github.com>
2023-03-17 13:25:52 +0100

    Tilføjet Tim Vandværk

--- config/config.js
+++ config/config.js
@@ -588,6 +588,28 @@ module.exports = {
                     }
                 }
             },
+            "vandvaerk_timvand2": { // Your print templates. "print" is the default
+                A4: {
+                    l: {
+                        mapsizePx: [1060, 730],
+                        mapsizeMm: [280, 192]
+                    },
+                    p: {
+                        mapsizePx: [730, 1060],
+                        mapsizeMm: [192, 280]
+                    }
+                },
+                A3: {
+                    l: {
+                        mapsizePx: [1525, 1065],
+                        mapsizeMm: [401, 282]
+                    },
+                    p: {
+                        mapsizePx: [1065, 1525],
+                        mapsizeMm: [282, 401]
+                    }
+                }
+            },
             "vandvaerk_hvamvandaalestrup2": { // Your print templates. "print" is the default
                 A4: {
                     l: {
