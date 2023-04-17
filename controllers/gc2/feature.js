f2321b1bbf3696e1100b55a0f8aa5ad546d38971
Martin Høgh <mh@mapcentia.com>
2023-04-17 10:29:11 +0200

    Any route instead of one for every method used

--- controllers/gc2/feature.js
+++ controllers/gc2/feature.js
@@ -1,71 +1,17 @@
 /*
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2018 MapCentia ApS
+ * @copyright  2013-2022 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  */
 
-var express = require('express');
-var router = express.Router();
-var config = require('../../config/config.js').gc2;
-var request = require('request');
-let app = express();
+const express = require('express');
+const router = express.Router();
+const config = require('../../config/config.js').gc2;
+const request = require('request');
 
-router.put('/api/feature/:db/:layer/:srs', function (req, response) {
-    var db = req.params.db, layer = req.params.layer, srs = req.params.srs, body = req.body, userName;
-
-    // Check if user is a sub user
-    if (req.session.screenName && req.session.subUser) {
-        userName = req.session.screenName + "@" + db;
-    } else {
-        userName = db;
-    }
-
-    var uri = "/api/v2/feature/" + userName + "/" + layer + "/"  + srs;
-
-    let headers = {
-        'GC2-API-KEY': req.session.gc2ApiKey
-    }
-    if (app.get('env') === 'test') {
-        headers.Cookie = "XDEBUG_SESSION=XDEBUG_SESSION;";
-    }
-
-    var options = {
-        method: 'PUT',
-        uri: config.host + uri,
-        json: body,
-        headers
-    };
-
-    console.log(body)
-
-    request(options, function (err, res, body) {
-
-        console.log(res.statusCode)
-
-        if (err || res.statusCode !== 200) {
-
-            response.header('content-type', 'application/json');
-            response.status(500).send({
-                success: false,
-                message: body
-            });
-
-            return;
-        }
-
-        response.header('content-type', 'application/json');
-        response.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-        response.header('Expires', '0');
-        response.header('X-Powered-By', 'MapCentia Vidi');
-
-        response.send(body);
-    });
-
-
-});
-
-router.post('/api/feature/:db/:layer/:srs', function (req, response) {
-    var db = req.params.db, layer = req.params.layer, srs = req.params.srs, body = req.body, userName;
+router.all('/api/feature/:db/:layer/:param', function (req, response) {
+    const db = req.params.db, layer = req.params.layer, param = req.params.param, body = req.body;
+    let userName;
 
     // Check if user is a sub user
     if (req.session.screenName && req.session.subUser) {
@@ -73,94 +19,29 @@ router.post('/api/feature/:db/:layer/:srs', function (req, response) {
     } else {
         userName = db;
     }
-
-    var uri = "/api/v2/feature/" + userName + "/" + layer + "/"  + srs;
-
-    var options = {
-        method: 'POST',
+    const uri = "/api/v2/feature/" + userName + "/" + layer + "/4326/"  + param;
+    const options = {
+        method: req.method,
         uri: config.host + uri,
         json: body,
         headers: {
-            'GC2-API-KEY': req.session.gc2ApiKey
+            'Cookie': "XDEBUG_SESSION=XDEBUG_SESSION;PHPSESSID=" + req.session.gc2SessionId,
         }
     };
-
-    console.log(body)
-
     request(options, function (err, res, body) {
-
-        console.log(res.statusCode)
-
         if (err || res.statusCode !== 200) {
-
             response.header('content-type', 'application/json');
             response.status(500).send({
                 success: false,
                 message: body
             });
-
             return;
         }
-
         response.header('content-type', 'application/json');
         response.header('Cache-Control', 'no-cache, no-store, must-revalidate');
         response.header('Expires', '0');
         response.header('X-Powered-By', 'MapCentia Vidi');
-
         response.send(body);
     });
-
-
-});
-
-router.delete('/api/feature/:db/:layer/:gid', function (req, response) {
-    var db = req.params.db, layer = req.params.layer, gid = req.params.gid, body = req.body, userName;
-
-    // Check if user is a sub user
-    if (req.session.screenName && req.session.subUser) {
-        userName = req.session.screenName + "@" + db;
-    } else {
-        userName = db;
-    }
-
-    var uri = "/api/v2/feature/" + userName + "/" + layer + "/4326/"  + gid;
-
-    console.log(uri)
-
-    var options = {
-        method: 'DELETE',
-        uri: config.host + uri,
-        json: body,
-        headers: {
-            'GC2-API-KEY': req.session.gc2ApiKey
-        }
-    };
-
-    console.log(body)
-
-    request(options, function (err, res, body) {
-
-        console.log(res.statusCode)
-
-        if (err || res.statusCode !== 200) {
-
-            response.header('content-type', 'application/json');
-            response.status(500).send({
-                success: false,
-                message: body
-            });
-
-            return;
-        }
-
-        response.header('content-type', 'application/json');
-        response.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-        response.header('Expires', '0');
-        response.header('X-Powered-By', 'MapCentia Vidi');
-
-        response.send(body);
-    });
-
-
 });
 module.exports = router;
