2572dff9590afb61533ccdacb96cd8ac7595de99
Martin Høgh <mh@mapcentia.com>
2024-01-09 12:13:53 +0100

    Added en_UK .po files

--- Gruntfile.js
+++ Gruntfile.js
@@ -283,7 +283,7 @@ module.exports = function (grunt) {
         },
         shell: {
             buildDocs: {
-                command: '. ./venv/bin/activate && sphinx-build ./docs ./docs/html'
+                command: '. ./venv/bin/activate && sphinx-build ./docs ./docs/_build/html'
             }
         },
         cacheBust: {
