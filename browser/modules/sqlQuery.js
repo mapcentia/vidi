ee3ec87c90723d11133153778ef62bcac5adeb7c
giovanniborella <rgb@mapster.dk>
2024-08-22 21:30:12 +0200

    Fix editing again..

--- browser/modules/sqlQuery.js
+++ browser/modules/sqlQuery.js
@@ -73,8 +73,7 @@ let editToolsHtml = `
 `;
 
 let editToolsHtmlNoBack = `
-<div class="form-group gc2-edit-tools" data-edi
-t-layer-id="{{_vidi_edit_layer_id}}" data-edit-layer-name="{{_vidi_edit_layer_name}}" data-edit-vector="{{_vidi_edit_vector}}" style="display: {{_vidi_edit_display}};">
+<div class="form-group gc2-edit-tools" data-edit-layer-id="{{_vidi_edit_layer_id}}" data-edit-layer-name="{{_vidi_edit_layer_name}}" data-edit-vector="{{_vidi_edit_vector}}" style="display: {{_vidi_edit_display}};">
     <div class="btn-group mt-1 w-100 mb-2">
         <button class="btn btn-outline-secondary btn-sm popup-edit-btn">
             <i class="bi bi-pencil" aria-hidden="true"></i>
