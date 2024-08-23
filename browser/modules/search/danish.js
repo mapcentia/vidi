40a823dabc0542d206247d10d4efbd25c427b3a5
René Giovanni Borella <rgb@mapster.dk>
2024-08-23 08:49:31 +0200

    Placeholder text assign (#51)
    * set placeholder on seach text input element
    * generalizing ...
    
    ---------
    
    Co-authored-by: Gunnar <gjj@geopartner.dk>

--- browser/modules/search/danish.js
+++ browser/modules/search/danish.js
@@ -94,18 +94,17 @@ module.exports = {
         let placeholder = window.vidiConfig?.searchConfig?.placeholderText;
         if (placeholder) {
             searchTxt = placeholder;
-            $(".custom-search.tt-input").attr("placeholder",
-                searchTxt
-            );
+            $(".custom-search").attr("placeholder",searchTxt);
+           
         } else {
             searchTxt = "Adresse, matr. nr.";
             if (sfeSearchActive) {
-                $(".custom-search.tt-input").attr("placeholder",
+                $(".custom-search").attr("placeholder",
                     searchTxt
                     + (esrSearchActive ? ", ESR nr. " : "")
                     + " eller SFE nr.");
             } else if (esrSearchActive) {
-                $(".custom-search.tt-input").attr("placeholder",
+                $(".custom-search").attr("placeholder",
                     searchTxt + " eller ESR nr.");
             }
         }
