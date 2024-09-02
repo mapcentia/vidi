4cda897726623855c9de81a1192ed073033b1a2d
Gunnar <gjj@geopartner.dk>
2024-09-02 11:42:44 +0200

    Merge branch 'master' into basemap-group

--- browser/modules/init.js
+++ browser/modules/init.js
@@ -45,6 +45,7 @@ module.exports = {
         const defaults = {
             schemata: [],
             baseLayers: [],
+            baseLayerGroups: [],
             autoPanPopup: false,
             crossMultiSelect: false,
             brandName: '',
