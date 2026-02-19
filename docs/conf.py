2833a5685f6231a84209009fe590da734f80bdf7
Martin Høgh <mh@mapcentia.com>
2026-02-19 09:46:37 +0100

    Update Python version to 3.13 in `.readthedocs.yaml` and remove unused gettext settings in Sphinx `conf.py`.

--- docs/conf.py
+++ docs/conf.py
@@ -264,7 +264,3 @@ texinfo_documents = [
 
 gettext_uuid = True
 gettext_compact = False
-
-locale_dirs = ["locales"]   # relativt til conf.py (altså ./docs/locale)
-gettext_compact = False    # anbefalet, så der bliver LC_MESSAGES-struktur
-
