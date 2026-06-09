76eadbcfcf0742adc165142a8163cae773c45ca5
Martin Høgh <mh@mapcentia.com>
2026-06-09 12:32:37 +0200

    refactor(legend): replace `request` with `fetch`, simplify async handling
    Switch from `request` to modern `fetch` API, leveraging `express-async-errors` for centralized async error handling. Streamline error checks and parsing logic for improved readability.

--- controllers/gc2/legend.js
+++ controllers/gc2/legend.js
@@ -1,48 +1,43 @@
 /*
  * @author     Martin Høgh <mh@mapcentia.com>
- * @copyright  2013-2018 MapCentia ApS
+ * @copyright  2013-2026 MapCentia ApS
  * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
  */
 
-var express = require('express');
-var router = express.Router();
-var config = require('../../config/config.js').gc2;
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const config = require('../../config/config.js').gc2;
 
-router.get('/api/legend/:db', function (req, response) {
+router.get('/api/legend/:db', async function (req, response) {
 
-    var l = req.query.l, db = req.params.db, url;
+    const l = req.query.l, db = req.params.db;
+    const url = config.host + "/api/v1d/legend/json/" + db + "?l=" + encodeURIComponent(l);
 
-    url = config.host + "/api/v1/legend/json/" + db + "?l=" + encodeURIComponent(l);
-
-    var options = {
-        uri: url,
-        encoding: 'utf8',
+    // Network errors reject here and are forwarded to the central
+    // error handler by express-async-errors (async handler).
+    const res = await fetch(url, {
         headers: {
             Cookie: "PHPSESSID=" + req?.session?.gc2SessionId
         }
-    };
+    });
 
-    request.get(options,
-        function (err, res, body) {
-            if (err) console.error(err);
-            let error = false;
-            try {
-                JSON.parse(body);
-            } catch (e) {
-                error = true;
-                console.error(e);
-            }
-            if (res.statusCode !== 200 || error) {
-                response.header('content-type', 'application/json');
-                response.status(400).send({
-                    success: false,
-                    message: "Could not get the legend data."
-                });
-                return;
-            }
-            response.send(JSON.parse(body));
-        }
-    )
+    const body = await res.text();
+    let data, parseError = false;
+    try {
+        data = JSON.parse(body);
+    } catch (e) {
+        parseError = true;
+    }
+
+    if (!res.ok || parseError) {
+        response.header('content-type', 'application/json');
+        response.status(400).send({
+            success: false,
+            message: "Could not get the legend data."
+        });
+        return;
+    }
+
+    response.send(data);
 });
 module.exports = router;
